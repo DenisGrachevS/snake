33867540f187874d16331f428fa622f2a928c05c
DenisGrachevS <14svarg@mail.ru>
2016-03-28 18:44:05 +0300

    Горизонтальные и вертикальные линии в классах

--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -10,10 +10,10 @@ namespace Snake
     {
         List<Point> pList;
 
-        public VerticalLine (int yLeft, int yReight, int x, char sym)
+        public VerticalLine (int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
-            for (int y = yLeft; y <= yReight; y++) 
+            for (int y = yUp; y <= yDown; y++) 
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
